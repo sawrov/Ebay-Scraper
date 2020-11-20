42d087bb112b91c07f6df50670a9e567859e2cb6
sawrov <sawrov@hotmail.com>
2020-11-20 12:19:51 +0800

    Retireved Missing Info except reviews

--- ebay_scraper.py
+++ ebay_scraper.py
@@ -12,11 +12,7 @@ class EbayScraper:
         self.select_variations = []
         self.images_to_download = []
         self.driver = driver
-        self.base_price = self.driver.find_element_by_xpath('//span[@itemprop="price"]').text
-        self.title = self.driver.find_element_by_id('itemTitle').text
-        self.shipping = self.driver.find_element_by_id('fshippingCost').text
-        self.details = self.driver.find_element_by_id('viTabs_0_is').text
-        self.seller = self.driver.find_element_by_xpath("//div[@class='bdg-90']/div/a/span").text
+        self.get_basic_info()
         self.writer=self.spreadsheet()
 
 
@@ -27,15 +23,38 @@ class EbayScraper:
         csv_writer = csv.writer(sheet)
         return csv_writer
 
+    def get_basic_info(self):
+        self.base_price = self.get_price()
+        self.title = self.driver.find_element_by_id('itemTitle').text
+        self.shipping = self.driver.find_element_by_id('fshippingCost').text
+        self.details = self.driver.find_element_by_id('viTabs_0_is').text
+        self.seller = self.driver.find_element_by_xpath("//div[@class='bdg-90']/div/a/span").text
+        self.item_location = self.driver.find_element_by_xpath('//span[@itemprop="availableAtOrFrom"]').text
 
-    def get_info(self):
-        print("\n\n")
+    def display_info_summary(self):
         print("----------Information Extracted-------------")
         print("TITLE:" + self.title)
         print("BASE PRICE:" + self.base_price)
-        print("SELLER: "+self.seller)
-        print("-----------sizes-available-------------")
+        print("SELLER: " + self.seller)
+        print("SHIPPING COST:" + self.shipping)
+        print("ITEM LOCATION:" + self.item_location)
+
+    def get_info(self):
+        print("\n\n")
+
         self.prepare_variations()
+
+
+        print("--------------SUMMARY-----------------\n")
+        self.display_info_summary()
+
+        print("\n\n-------DESCRIPTION---------\n")
+        self.get_description()
+
+        print("\n\n----------------POSTAGE-INFORMATION---------------")
+        self.get_postage_info()
+
+        print("\n-------Check Mother CSV for Variations----------")
 
         # Uncomment  lines below after completion
 
@@ -57,6 +76,34 @@ class EbayScraper:
             self.images_to_download.append(image.get_attribute('src'))
             print(image.get_attribute('src'))
 
+    # //*[@id="viTabs_0_is"]/div/table/tbody/tr[1]
+    #Function to get all the information from the table within the description tab.
+    def get_description(self):
+        self.driver.execute_script("arguments[0].scrollIntoView(true);",
+                                   self.driver.find_element_by_id("viTabs_0_is"))
+
+        tabledata = self.driver.find_elements_by_xpath('//div[@id="viTabs_0_is"]/div/table/tbody/tr')
+        for row in tabledata:
+            item=lambda x: row.find_element_by_xpath('./td['+str(x)+']').text
+            print(item(1)+" "+item(2)+"\n"+item(3)+" "+item(4))
+
+    #Extracting information from postage tab. Might have to change the id of the tab if I discover more tabs and dynamic tab naming.
+
+    def get_postage_info(self):
+        self.driver.execute_script("arguments[0].click();",
+                                   self.driver.find_element_by_id("viTabs_1"))
+
+        self.ship_to = self.driver.find_element_by_xpath('//select[@id="shCountry"]/option[@value>1 and @selected]').text
+        print("Shipping to: " + self.ship_to)
+
+        shipping_table = self.driver.find_elements_by_xpath('//div[@id="shippingSection"]/table/tbody/tr')
+        for i,row in enumerate(shipping_table):
+            print("\n")
+            print("SHIPPING OPTION:"+str(i))
+            item = lambda x: row.find_element_by_xpath('./td[' + str(x) + ']').text
+            print(item(1) + " " + item(2) + "\n" + item(3) + "\n" + item(4))
+            print("--------------------------------")
+
     def get_number_of_variation(self):
         i = 1
         try:
@@ -73,10 +120,9 @@ class EbayScraper:
     def recursive_enumeration(self,information, depth):
         data = information[depth]
         if(len(information)>(depth+1)):
-            print("HERE")
             depth=depth+1
             for info in data[1]:
-                print("\t" +data[2] +":"+info)
+                #print("\t" +data[2] +":"+info)
                 try:
                     self.current_recursive_item=info
                     data[0].select_by_visible_text(info)
@@ -88,7 +134,7 @@ class EbayScraper:
             for info in data[1]:
                 try:
                     data[0].select_by_visible_text(info)
-                    print("\t\t"+data[2]+info+"Price :"+self.get_price())
+                    #print("\t\t"+data[2]+info+"Price :"+self.get_price())
                     self.writer.writerow(["URL",self.title,self.seller,self.current_recursive_item,info,self.get_price()])
                 except exceptions.NoSuchElementException:
                     pass
